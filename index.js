f73c2e9f6d4f598de698f3f43104a71bf735c599
boisterous-character-0677 <ramigreenspan@gmail.com>
2019-02-27 13:58:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ function whisper (string){
 function logShout (string){
   console.log ("${string}")
   var uppercase = "HELLO!"
-uppercase.toUpperCase === uppercase 
+uppercase.toUpperCase() === uppercase 
 }
